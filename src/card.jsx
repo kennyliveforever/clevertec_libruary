28bf0b3e3c1c4b33fdd9f38fd43300bfa987c684
kennyliveforever <gordon_freeman@live.ru>
2023-01-26 22:38:35 +0300

    kukuruza!

--- src/card.jsx
+++ src/card.jsx
@@ -1,69 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './card.css'
 
-//
+export default function Card(props) {
 
-// function Stars() {
-//     if (rate) {
-//         return(
-//             <div className={'rate'}>
-//                 <div className={'star'}></div>
-//                 <div className={'star'}></div>
-//                 <div className={'star'}></div>
-//                 <div className={'star'}></div>
-//                 <div className={'star'} id={'star-empty'}></div>
-//             </div>
-//         )
-//     }
-//     return (
-//         <div className={'rate'}>
-//             ещё нет оценок
-//         </div>
-//     )
-// }
-// function Stars(props) {
-//     if (Card.props.rate === true) {
-//         return (
-//             <>
-//                 <div className={'rate'}>
-//                     <div className={'star'}></div>
-//                     <div className={'star'}></div>
-//                     <div className={'star'}></div>
-//                     <div className={'star'}></div>
-//                     <div className={'star'} id={'star-empty'}></div>
-//                 </div>
-//             </>
-//         )
-//     }
-// }
-
-
-
-export default function Card(props, {children}) {
     let Stars
+    let buttonStyle
+    let buttonTextStyle
+
     if (props.rate) {
             Stars = <div className={'rate'}>
-            <div className={'star'}></div>
-            <div className={'star'}></div>
-            <div className={'star'}></div>
-            <div className={'star'}></div>
-            <div className={'star'} id={'star-empty'}></div>
+            <div className={'star'}/>
+            <div className={'star'}/>
+            <div className={'star'}/>
+            <div className={'star'}/>
+            <div className={'star'} id={'star-empty'}/>
         </div>
     } else {
         Stars = <div className={'rate'}>
             ещё нет оценок
         </div>
     }
+
+    if (props.order === 'Забронирована') {
+        buttonStyle = {background: 'none', border: '1px solid #BFC4C9'}
+        buttonTextStyle = {color: '#363636'}
+    }
+
+    if (props.order !== 'Забронирована' && props.order !== 'Забронировать') {
+        buttonStyle = {background: '#F9F9FA', border: '1px solid #BFC4C9'}
+        buttonTextStyle = {color: '#A7A7A7'}
+    }
+
         return (
             <div className={'card-box'}>
-                <div className={'card-picture'}></div>
+                <div className={'card-picture'} style={{backgroundImage: 'url('+props.image+')'}}/>
                 {Stars}
                 <div className={'description'}>
                     <div className={'book-title'}>{props.title}</div>
                     <div className={'book-author'}>{props.author}</div>
                 </div>
-                <div className={'order-button'}>
-                    <div className={'order-button-text'}>{props.order}</div>
+                <div className={'order-button'} style={buttonStyle}>
+                    <div className={'order-button-text'} style={buttonTextStyle}>{props.order}</div>
                 </div>
             </div>
         )
